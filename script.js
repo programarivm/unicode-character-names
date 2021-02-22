39db5afd44914fb7a7ebce6f2e5dc271fbe2057b
programarivm <info@programarivm.com>
2021-02-22 12:00:43 +0100

    Appended character description

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ const table = document.querySelector("table")
 fetch('./unicode.json')
   .then(response => response.json())
   .then(unicode => {
+    
     const addRow = (el, found, i) => {
       const newRow = el.insertRow(i)
-      const newCell  = newRow.insertCell(0)
-      const newText  = document.createTextNode(unicode[found.item])
-      newCell.appendChild(newText)
+      const cellChar  = newRow.insertCell(0)
+      const textChar  = document.createTextNode(unicode[found.item])
+      cellChar.appendChild(textChar)
+
+      const cellDescription  = newRow.insertCell(1)
+      const textDescription  = document.createTextNode(found.item)
+      cellDescription.appendChild(textDescription)
     }
 
     button.addEventListener("click", function(event) {
