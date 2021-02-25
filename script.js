fc9b6bd35e6f420990b4259226a61154e121f222
programarivm <info@programarivm.com>
2021-02-25 22:46:12 +0100

    Cleaned up script.js

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-import Fuse from './fuse.esm.min.js'
+import Fuse from './fuse.esm.min.js';
 
-const input = document.querySelector("input")
-const button = document.querySelector("button")
-const table = document.querySelector("table")
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const table = document.querySelector('table');
 
 fetch('./unicode.json')
   .then(response => response.json())
   .then(unicode => {
-
     const addRow = (el, found, i) => {
-      const newRow = el.insertRow(i)
-      const cellChar  = newRow.insertCell(0)
-      const textChar  = document.createTextNode(unicode[found.item])
-      cellChar.appendChild(textChar)
+      const newRow = el.insertRow(i);
+      const cellChar  = newRow.insertCell(0);
+      const textChar  = document.createTextNode(unicode[found.item]);
+      cellChar.appendChild(textChar);
 
-      const cellDescription  = newRow.insertCell(1)
-      const textDescription  = document.createTextNode(found.item)
-      cellDescription.appendChild(textDescription)
+      const cellDescription  = newRow.insertCell(1);
+      const textDescription  = document.createTextNode(found.item);
+      cellDescription.appendChild(textDescription);
     }
 
     const fuzzySearch = (e) => {
-      table.innerHTML = ''
+      table.innerHTML = '';
+
       const fuse = new Fuse(
         Object.keys(unicode), {
           minMatchCharLength: 3,
           threshold: 0.3,
           distance: 33
-      })
+      });
+
       fuse.search(input.value)
         .forEach((found, i) => {
           addRow(table, found, i)
@@ -35,11 +36,11 @@ fetch('./unicode.json')
 
     const inputSearch = (e) => {
       if (e.keyCode === 13) {
-        e.preventDefault()
-        fuzzySearch()
+        e.preventDefault();
+        fuzzySearch();
       }
     }
 
     input.addEventListener('keydown', inputSearch);
-    button.addEventListener("click", fuzzySearch)
-  })
+    button.addEventListener('click', fuzzySearch);
+});
